1570d66424fa02576b3a2fbf6e67bf177f660f43
Ricer-png <923107995@qq.com>
2021-03-24 16:48:37 +0800

    lalala

---
+++ resources/views/loyal.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>我也不知道写什么好</title>
+    </head>
+<body>
+	<p>这里随便</p>
+	<hr />
+	<p>随便</p>
+	<hr />
+	<p>这是段落。</p>
+	<hr />
+	<p>这是段落。。。。</p>
+</body>
+</html>
